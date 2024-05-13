e47038c8bb273e46cd8379ef5dc537d34cc2176d
regiin09 <regiinarulrithik@gmail.com>
2024-05-13 10:40:26 +0530

    'poda'

---
+++ Assets/Scene1.cs
@@ -0,0 +1,28 @@
+
+
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Scene1 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Text tt;
+    public Text inp;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void summa()
+    {
+        tt.text=inp.text;
+    }
+}
